af55f5a88ca0c5012c615e4fc7d8f7a7d0db271a
Alfredo Delgado L <aadelop@gmail.com>
2024-09-23 15:39:08 +0200

    Added logic that generate a pair of public and private key to allow encrypt messages

---
+++ generate_keys.js
@@ -0,0 +1,16 @@
+const { createECDH, createPublicKey } = require("crypto");
+const { exit } = require("process");
+const args = require("yargs").argv
+const fs = require("fs")
+console.log(args.name);
+
+if(!args.name){
+    console.log("please specify the --name argument to start the execution" );
+    exit(0)
+}
+
+const keyPair = createECDH("secp521r1")
+const publicKey = keyPair.generateKeys("hex")
+const privateKey = keyPair.getPrivateKey("hex")
+fs.writeFileSync("./data/"+args.name+".key", publicKey)
+fs.writeFileSync("./data/"+args.name+".pub", privateKey)
