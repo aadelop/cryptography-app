6b70a8b6d8860982f12fd271f28a417a3f1a56f8
Alfredo Delgado L <aadelop@gmail.com>
2024-09-24 07:16:03 +0200

    Completed logic of the program for encrypt a message with private and public key

---
+++ encrypt_file.js
@@ -0,0 +1,26 @@
+const {createCipher, createECDH, createCipheriv} = require("crypto")
+const args = require("yargs").argv
+const { exit } = require("process");
+const fs = require("fs")
+
+if(!args.private && !args.public && !args.file){
+    console.log("Please add --file, --private, --public, parameters to start encryption. ");
+}
+
+const origin = createECDH("secp521r1")
+const key = fs.readFileSync("./data/"+ args.private + ".key").toString()
+origin.setPrivateKey(key, "hex")
+
+const pub = fs.readFileSync("./data/" + args.public +".pub").toString()
+
+
+//Creation of SHARED secret key
+const secret = Uint8Array.from(origin.computeSecret(pub, "hex", 'binary'))
+
+//cipher of the file
+const algot = 'aes-256-cbc'
+var cipher = createCipheriv(algot, secret.slice(0,32), secret.slice(0,16))
+const file = fs.readFileSync("./data/" + args.file)
+let encrypted = cipher.update(file, 'utf-8', 'hex')
+encrypted += cipher.final("hex")
+fs.writeFileSync("./data/" + args.public + "-" + args.file + ".enc", encrypted)
